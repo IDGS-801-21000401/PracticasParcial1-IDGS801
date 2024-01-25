7f210ecdb2f411c3782cff95f87322d2032b9161
IDGS-801-21000401 <21000401@alumnos.utleon.edu.mx>
2024-01-24 18:53:27 -0600

    24/01/24 ARMC Ejercicio 3 de clase

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,33 +1,32 @@
-from flask import Flask,render_template,request
-app=Flask(__name__)
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
 
-@app.route("/OperasBas",methods=["GET","POST"])
+@app.route("/OperasBas", methods=["GET", "POST"])
 def operaBas():
     return render_template("OperasBas.html")
 
-@app.route("/resultado",methods=["GET","POST"])
+@app.route("/resultado", methods=["POST"])
 def result():
     if request.method == "POST":
-        suma = request.form.get("btnSuma")
-        resta = request.form.get("btnResta")
-        multiplicacion = request.form.get("btnMultiplicacion")
-        division = request.form.get("btnDivision")
+        operacion = request.form.get("operacion")
         num1 = request.form.get("n1")
         num2 = request.form.get("n2")
-    
-        if multiplicacion == "on":
-            return "La multiplicación de {} y {} = {}".format(num1, num2, str(int(num1)* int(num2)))   
-        if suma == "on":
-            return "La suma de {} y {} = {}".format(num1, num2, str(int(num1)+int(num2)))
-        if resta == "on":
-            return "La resta de {} y {} = {}".format(num1, num2, str(int(num1)-int(num2)))
-        if division == "on":
-            return "La division de {} y {} = {}".format(num1, num2, str(int(num1)/int(num2)))
-        
-        
 
-       
+        if operacion == "Suma":
+            return "La suma de {} + {} = {}".format(num1, num2, str(int(num1) + int(num2)))
+        elif operacion == "Resta":
+            return "La resta de {} - {} = {}".format(num1, num2, str(int(num1) - int(num2)))
+        elif operacion == "Multiplicacion":
+            return "La multiplicación de {} * {} = {}".format(num1, num2, str(int(num1) * int(num2)))
+        elif operacion == "Division":
+            if int(num2) != 0:
+                return "La división de {} / {} = {}".format(num1, num2, str(int(num1) / int(num2)))
+            else:
+                return "Error: División por cero"
+
+        # Agrega un retorno por defecto en caso de que la operación no sea reconocida
+        return "Operación no válida"
 
 if __name__ == "__main__":
     app.run(debug=True)
